bc4f3341f95513eb559ae4d2fe14665e44fdb621
EnkiduAI <dreddy1123@gmail.com>
2021-12-28 12:43:21 +0300

    Test modifiers

--- service/src/main/test/service/ProjectServiceTest.java
+++ service/src/main/test/service/ProjectServiceTest.java
@@ -24,16 +24,16 @@ import com.epam.esm.model.service.impl.ProjectServiceImpl;
 public class ProjectServiceTest {
 
 	@Mock
-	ProjectServiceImpl service;
-	List<GiftCertificate> certificates;
-	List<Tag> tags;
-	List<CertificateWithTag> certsWithTags;
-	GiftCertificate certificate;
-	Tag tag;
-	CertificateWithTag cwt;
+	private ProjectServiceImpl service;
+	private List<GiftCertificate> certificates;
+	private List<Tag> tags;
+	private List<CertificateWithTag> certsWithTags;
+	private GiftCertificate certificate;
+	private Tag tag;
+	private CertificateWithTag cwt;
 
 	@BeforeEach
-	void setUp() {
+	protected void setUp() {
 		certificate = new GiftCertificate(154, "testCert", "testCert", 111, "23", LocalDateTime.now(),
 				LocalDateTime.now());
 		tag = new Tag(66, "TEST");
@@ -45,56 +45,56 @@ public class ProjectServiceTest {
 	}
 
 	@Test
-	void findCertificates() throws ServiceException {
+	protected void findCertificates() throws ServiceException {
 		when(service.findAllCertificates()).thenReturn(certificates);
 		List<GiftCertificate> expected = service.findAllCertificates();
 		assertEquals(expected, certificates);
 	}
 	
 	@Test
-	void findTags() throws ServiceException{
+	protected void findTags() throws ServiceException{
 		when(service.findAllTags()).thenReturn(tags);
 		List<Tag> expected = service.findAllTags();
 		assertEquals(expected, tags);
 	}
 	
 	@Test
-	void findCertficatesWithTags() throws ServiceException {
+	protected void findCertficatesWithTags() throws ServiceException {
 		when(service.getCertificatesWithTags(anyString(), anyString(), anyString())).thenReturn(certsWithTags);
 		List<CertificateWithTag> expected = service.getCertificatesWithTags("TkjGG","c", "ASC");
 		assertEquals(expected, certsWithTags);
 	}
 	
 	@Test
-	void createCertificate() throws ServiceException{
+	protected void createCertificate() throws ServiceException{
 		when(service.createCertificate("testCert", "testCert", 111, "23")).thenReturn(certificate);
 		GiftCertificate expected = service.createCertificate("testCert", "testCert", 111, "23");
 		assertEquals(expected, certificate);
 	}
 	
 	@Test
-	void createTag() throws ServiceException{
+	protected void createTag() throws ServiceException{
 		when(service.createTag("TEST")).thenReturn(tag);
 		Tag expected = service.createTag("TEST");
 		assertEquals(expected, tag);
 	}
 	
 	@Test
-	void createCertificateWithTag() throws ServiceException{
+	protected void createCertificateWithTag() throws ServiceException{
 		when(service.createCertificate("TESTTAG", "CErtName", "SimpleDesc", 111, "23")).thenReturn(cwt);
 		CertificateWithTag expected = service.createCertificate("TESTTAG", "CErtName", "SimpleDesc", 111, "23");
 		assertEquals(expected, cwt);
 	}
 	
 	@Test
-	void findCertificateById()throws ServiceException{
+	protected void findCertificateById()throws ServiceException{
 		when(service.findCertificateById(154)).thenReturn(certificate);
 		GiftCertificate expected = service.findCertificateById(154);
 		assertEquals(expected, certificate);
 	}
 	
 	@Test
-	void findTagById() throws ServiceException{
+	protected void findTagById() throws ServiceException{
 		when(service.findTagById(66)).thenReturn(tag);
 		Tag expected = service.findTagById(66);
 		assertEquals(expected, tag);
